de0b32a6d8f01b73990f33dc0e6911d28f7012f5
josiahok <minwidth49@gmail.com>
2026-03-09 12:26:34 +0000

    updated with autoupate text changes

--- src/shared/TitleBar.tsx
+++ src/shared/TitleBar.tsx
@@ -286,7 +286,7 @@ const TitleBar = () => {
             <img
               src="./evsongsicon.png"
               alt="Description"
-              className="w-4 h-4 animate-bounce"
+              className="w-4 h-4 "
             />
           </div>
         </div>
